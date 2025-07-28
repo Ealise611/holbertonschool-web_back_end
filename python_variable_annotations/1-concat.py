df143bc4a18a7439a230f04363888befe836ba1d
Ealise <10503@holbertonstudents.com>
2025-07-28 20:59:41 +1000

    fix docstring

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -6,4 +6,7 @@ a string str1 and a string str2 as arguments and returns a concatenated string
 
 
 def concat(str1: str, str2: str) -> str:
+    """
+    function for str concat
+    """
     return str1 + str2
