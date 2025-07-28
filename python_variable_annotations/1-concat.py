a602feaae417ea566cefc40b18828e88c1a75161
Ealise <10503@holbertonstudents.com>
2025-07-28 20:59:09 +1000

    fix

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -4,5 +4,6 @@ Write a type-annotated function concat that takes
 a string str1 and a string str2 as arguments and returns a concatenated string
 """
 
-def concat(str1:str, str2:str) -> str:
+
+def concat(str1: str, str2: str) -> str:
     return str1 + str2
