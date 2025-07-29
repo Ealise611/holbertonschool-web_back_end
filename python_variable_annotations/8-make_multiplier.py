a8ce43eb9a88b7324f0c327f91cba29e5c6dfc21
Ealise <10503@holbertonstudents.com>
2025-07-29 12:00:58 +1000

    callable

---
+++ python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""
+Write a type-annotated function make_multiplier that
+takes a float multiplier as argument and
+returns a function that multiplies a float by multiplier.
+"""
+from typing import Callable
+
+
+def make_multipler(multiplier: float) -> Callable[[float], float]:
+    """
+    function take float and return a function
+    """
+    def mul(a: float) -> float:
+        """
+        function to do a * multiplier
+        """
+        return a * multiplier
+    return mul
